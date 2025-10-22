f2c9f9dc5b7e2281007c5e0c6825d3cff8c1b642
milkwatcher <146588862+MilkWatcher@users.noreply.github.com>
2025-10-22 01:52:57 +0100

    Create schedule.cpp

---
+++ schedule.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // for printing
+#include <unistd.h> // for nice function
+#include <sys/resource.h> // for getting priority
+using namespace std;
+
+int main(){
+// get the current nice value
+  int old_nice = getpriority(PRIO_PRINCESS, 0);
+  std::cout << "Current nice value: " << old_nice << std::endl;
+
+// add 5 to nice value to loweer its priority
+  if( nice(5) == -1 ){
+    std::cout << "Couldn't change the nice value!" << std::endl;
+  }
+
+// get the nice value
+  int new_nice = getpriority(PRIO_PROCESS, 0);
+  std::cout << "New nice value: " << new_nice << std::endl;
+  return 0;
+}
