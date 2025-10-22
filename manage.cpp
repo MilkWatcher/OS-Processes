b7448e14017742607fb874ac9c275d2959f8927a
milkwatcher <146588862+MilkWatcher@users.noreply.github.com>
2025-10-22 01:30:41 +0100

    Create manage.cpp

---
+++ manage.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // for printing
+#include <unistd.h> // for getting process IDs
+
+int main(){
+// print the current process ID
+  std::cout << "My process ID: " << getpid() << std::endl;
+// print parent process ID
+  std::cout << "My parent process ID: " << getppid() << std:endl;
+  return 0;
+}
