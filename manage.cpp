70bdfe19187adf3588158d7e7feac741d0a1fcbe
milkwatcher <146588862+MilkWatcher@users.noreply.github.com>
2025-11-05 06:52:17 +0000

    Fix typo in output for parent process ID

--- manage.cpp
+++ manage.cpp
@@ -5,6 +5,6 @@ int main(){
 // print the current process ID
   std::cout << "My process ID: " << getpid() << std::endl;
 // print parent process ID
-  std::cout << "My parent process ID: " << getppid() << std:endl;
+  std::cout << "My parent process ID: " << getppid() << std::endl;
   return 0;
 }
