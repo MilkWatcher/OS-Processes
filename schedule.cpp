84c074f95379d20506af0b1c24e8c34ed780fa84
milkwatcher <146588862+MilkWatcher@users.noreply.github.com>
2025-11-05 06:54:04 +0000

    Update schedule.cpp

--- schedule.cpp
+++ schedule.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main(){
 // get the current nice value
-  int old_nice = getpriority(PRIO_PRINCESS, 0);
+  int old_nice = getpriority(PRIO_PROCESS, 0);
   std::cout << "Current nice value: " << old_nice << std::endl;
 
 // add 5 to nice value to loweer its priority
   if( nice(5) == -1 ){
     std::cout << "Couldn't change the nice value!" << std::endl;
+    retun -1;
   }
 
 // get the nice value
